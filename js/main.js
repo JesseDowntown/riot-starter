fa25a0e251ae2b9c51530dbb39119d315f093f71
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-12-11 15:02:33 -0600

    Add additional items with riot.js.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-alert('hi');
+var itemTemplate = $('#templates .item').html();
+
+var newItemHtml = $.render(itemTemplate, { name: 'Dewberry', price: 0.15 });
+$('#store').append(newItemHtml);
+
+var items = [
+  { name: 'lemon', price: 3},
+  { name: 'orange', price: 6},
+  { name: 'artichoke', price: 9},
+]
+
+
+for (var i = 0; i < items.length; i++ ) {
+    var moreItem = $.render(itemTemplate, items[i]);
+    $('#store').append(moreItem);
+  };
+
+
+
