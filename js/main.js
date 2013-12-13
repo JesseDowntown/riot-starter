4bb44cff4af2df83132b2f4ac619d2a7c3b6439f
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-12-13 13:31:22 -0600

    commit to commiting

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-alert('hi');
+$('#button').on('click', function(e) {
+  var item = $('#name').val();
+  var cost = $('#price').val();
+  var newItemHtml = $.render(newItem, { name: item, price: cost});
+  $('#store').append(newItemHtml);
+  e.preventDefault();
+})
+
+var newItem = $('#templates .item').html()
