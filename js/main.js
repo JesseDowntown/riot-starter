54349d5f8737d5310f6ad890e2a0558e845f9de2
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-12-13 17:28:29 -0600

    Create javascript method to render genre stats.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ var videos = [
   { title: 'Sadness', youtubeId: 'sIeJSSjTG3k', genre: 'movie clip' }
 ];
 // Pull html from index and return it as a string
-var itemTemplate = $('#templates .video-list-item').html()
+var itemTemplate = $('#templates .video-list-item').html();
+var statsTemplate = $('#templates .genre-stats').html();
 // Interpolate data and render to html page using riot.js
 var renderVideoList = function () {
   for (var i = 0; i < videos.length; i++) {
@@ -18,9 +19,11 @@ renderVideoList();
 $('#submit').on('click', function(e) {
   var title = $('#createTitle').val();
   var youtubeId = $('#createYoutubeId').val();
+  var genre = $('#createGenre').val();
   videos.push({});
   videos[(videos.length)-1].title = title;
   videos[(videos.length)-1].youtubeId = youtubeId;
+  videos[(videos.length)-1].genre = genre;
   var newItemHtml = $.render(itemTemplate, videos[videos.length-1]);
   $('#video-list').append(newItemHtml);
   e.preventDefault();
@@ -36,6 +39,22 @@ var calculateGenreStats = function () {
     stats[genre] += 1;
   }
 }
-calculateGenreStats();
-console.log(stats);
+// Render genre stats
+var renderGenreStats = function() {
+  calculateGenreStats();
+  for (var genre in stats) {
+    var stat = {};
+    stat.number = stats[genre];
+    stat.genre = genre
+    console.log(stat);
+    var newStatsHtml = $.render(statsTemplate, stat);
+    $("#genre-stats").append(newStatsHtml)
+  }
+}
+
+$('#stat-button').on('click', function () {
+  $("#genre-stats").empty();
+  renderGenreStats();
+})
+
 
